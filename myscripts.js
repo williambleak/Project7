46a57a5cf5ee65bea0b6b90b560f705960ca98e3
williambleak <wb042238@stu.provo.edu>
2023-11-20 15:02:51 -0700

    Finished

--- myscripts.js
+++ myscripts.js
@@ -1,17 +1,17 @@
 var imgArray = new Array(
-    'Project6/Images/Fullsize/duckLg.jpg',
-    'Project6/Images/Fullsize/HeronLg.jpg',
-    'Project6/Images/Fullsize/PalicanLg.jpg',
-    'Project6/Images/GeeseLg.jpg'
+    'duckLg.jpg',
+    'HeronLg.jpg',
+    'PalicanLg.jpg',
+    'GeeseLg.jpg'
     );
-    var titleArray = new Array(
+var titleArray = new Array(
     'Duck',
     'Heron',
     'Palican',
     'Geese'
     );
-    var imgPath = "Images/Fullsize/";
-    function swapImage(imgID) {
+var imgPath = "Images/fullsize/";
+function swapImage(imgID) {
     var theImage = document.getElementById('theImage');
     var textDiv = document.getElementById('bottomText');
     var newImg;
@@ -21,9 +21,9 @@ var imgArray = new Array(
     textTitle=titleArray[imgID];
     textDiv.innerHTML = textTitle;
     }
-    function preloadImages() {
+function preloadImages() {
     for(var i = 0; i < imgArray.length; i++) {
-    var tmpImg = new Image;
-    tmpImg.src = imgPath + imgArray[i];
+        var tmpImg = new Image;
+        tmpImg.src = imgPath + imgArray[i];
     }
     }
